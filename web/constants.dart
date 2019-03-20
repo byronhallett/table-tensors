115a1248defb3b03f70cbd138204895f51731924
Byron Hallett <byron.james.h@gmail.com>
2019-03-20 15:26:52 +1100

    start dart version

---
+++ web/constants.dart
@@ -0,0 +1,9 @@
+// CONSTANTS
+class Color {
+  static const MAUVE = 0xae7178;
+  static const TEAL = 0x71aea7;
+  static const WHITE = 0xFFFFFF;
+}
+class PHYSICS {
+  static const double PADDLE_SPEED = 0.025;
+}
---
+++ web/dimensions.dart
@@ -0,0 +1,8 @@
+class Dimensions {
+
+
+
+  Dimensions() {
+
+  }
+}
